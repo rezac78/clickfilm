8a47ecd76bb2439e48d70d623fee949087bbaa91
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2023-03-20 15:57:15 +0330

    ok

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
-import Link from 'next/link'
 export default function Footer() {
+        const DateNew = new Date()
+        const DateClickFilm = DateNew.toLocaleDateString().slice(5, 10)
         return (
                 <>
                         <div className="Footer text-center">
-                                <span>ClickFilm 2021</span>
+                                <span>ClickFilm {DateClickFilm}</span>
                         </div>
                 </>
         )
