dec8d60ee86869e146366473511dd29154c0d6d0
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2023-03-27 17:48:05 +0330

    ok

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,7 +3,6 @@ import Link from 'next/link'
 export default function Card({ poster, summary, name }) {
         return (
                 <>
-
                         <div className="card" style={{ "background": "black" }}>
                                 <div className="card-content">
                                         <h2>{name}</h2>
@@ -12,7 +11,6 @@ export default function Card({ poster, summary, name }) {
                                 </div>
                                 <Image width={500} height={100} src={poster} alt="" className="award-img" />
                         </div>
-
                 </>
         )
 }
