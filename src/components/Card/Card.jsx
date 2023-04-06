24c63726e6073fc2b1e9353628379578bb050097
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2023-04-06 15:35:15 +0330

    getStaticProps

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,47 @@
+import { useState, useRef } from "react";
 import Image from 'next/image'
 import Link from 'next/link'
-export default function Card({ poster, summary, name }) {
+export default function Card({ poster, summary, name, videoPoster }) {
+        const [isActive, setIsActive] = useState(false);
+        const vidRef = useRef(null);
+        const myLoader = ({ src }) => {
+                return poster;
+        };
+        const handleClick = () => {
+                setIsActive(!isActive);
+                var Video = vidRef.current;
+                Video.pause();
+                Video.currentTime = 0;
+        };
         return (
                 <>
                         <div className="card" style={{ "background": "black" }}>
                                 <div className="card-content">
                                         <h2>{name}</h2>
                                         <p className="">{summary}</p>
-                                        <Link href="/Download">دانلود فیلم</Link>
-                                </div>
-                                <Image width={600} height={100} src={poster} alt="Poster" className="award-img" />
+                                        <div className="BannerTrailer">
+                                                <div className="BannerTrailer-Button">
+                                                        <Link href="#" onClick={handleClick}>
+                                                                تماشا
+                                                        </Link>
+                                                </div>
+                                                <div
+                                                        className={`trailer ${isActive ? "active" : ""}`}
+                                                >
+                                                        <video ref={vidRef} controls={true}>
+                                                                <source type="video/webm" src={videoPoster} />
+                                                        </video>
+                                                        <Image
+                                                                width={100}
+                                                                height={10}
+                                                                src="/img/close.png"
+                                                                className="close"
+                                                                onClick={handleClick}
+                                                                alt="close"
+                                                        />
+                                                </div>
+                                        </div>                                </div>
+                                <Image width={600} height={100} loader={myLoader} src={poster} alt="Poster" className="award-img" />
                         </div>
                 </>
         )
--- src/pages/BW.tsx
+++ src/pages/BW.tsx
@@ -1,16 +1,53 @@
 import Card from "../components/Card/Card"
-import { CardNetflix } from "../events/events"
 import Headers from "../components/Header/header"
 import Meta from "@/components/MetaTitle/Meta"
 import Footer from "@/components/Footer/Footer"
-export default function BW() {
+import { GraphQLClient, gql } from "graphql-request";
+export const getStaticProps = async () => {
+        const url =
+                "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clfze5ufq6z0t01ui0x1zan9t/master";
+        const graphQLClient = new GraphQLClient(url, {
+                headers: {
+                        Authorization:
+                                "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ2ZXJzaW9uIjozLCJpYXQiOjE2ODA1MTMwODQsImF1ZCI6WyJodHRwczovL2FwaS1ldS1jZW50cmFsLTEtc2hhcmVkLWV1YzEtMDIuaHlncmFwaC5jb20vdjIvY2xmemU1dWZxNnowdDAxdWkweDF6YW45dC9tYXN0ZXIiLCJtYW5hZ2VtZW50LW5leHQuZ3JhcGhjbXMuY29tIl0sImlzcyI6Imh0dHBzOi8vbWFuYWdlbWVudC5ncmFwaGNtcy5jb20vIiwic3ViIjoiOWUwNzMwMGItMDJiYy00M2Y3LWExYzEtOTA3ZDZiNWMzMDRkIiwianRpIjoiY2xnMG0zbjZrN240eDAxdWkzYWRvZ29haiJ9.WS1Sb39th6FrsqyjOAjjHsvm_M8OqQmcork0jd-prcH-eU-N_96dBvhiIWKA74ikk-4u4UM1BRcyUGBVvjDKdQH1499eJH2Qmqg5GWd9YdI_ysC9KSJkX4-YlyHPwdIttH9zMKnKGVcPcyve7cZGRB8CQ-OIPr31mr0n8tZ-bHoIykDbCh4hZtvIGKYztE-JfWzYevDchC-NoR461_Toa-YggbUVyJ4S3Rg7P-fzl7HeD7ke3OVhbfNkOKgMjFczySud7O0BVrIKYr5_z0DEMtn2XUoxHqMBN41zjvZYajvgP_w_FWpagzrpFAt8xesCwMWiy3EQedwjR31nU9LKu9Nq6RSKPwRH4IGFiKohhBuDkDA26GnNVO2v5gftGovOiK2fqU76jmB0jm4mRMiQ2za8bRxGv_8I6eoT6mxbAm0bwvH2C1YVfEcYAq7uh65MLv80Gc0KYCUsgbT7WcXv69N2Nbrod_r-7_1-WBFUE49pYXWPwrzBdnNXKa-V6qwxFTBXRI6nDzxTA1LjK3i0HnQ_3NDHXxLVRTNYDJ---NdKhUlIpyuSr9oW6LROTO-4gYs5JcDbkp_fPaX8b654hVXLw1XV6tbIVS9_oHZtRQSFc5vknkCZYCcAc6vjvm4hqFooQOBplNsjIaEy_95IqyeYlMP3JfU7YSUv9qshymE",
+                },
+        });
+        const query1 = gql`
+        query {
+                wbs {
+                        createdAt,
+                        id,
+                        title,
+                        description,
+                        mp4{
+                        url
+                        },
+                        poster {
+                        url
+                        }
+                }
+        }
+  `;
+        const data: any = await graphQLClient.request(query1);
+        const BW = data.wbs
+        return {
+                props: {
+                        BW,
+                }
+        }
+}
+interface PageProps {
+        BW: any;
+}
+export default function BW(props: PageProps) {
+        const BW = props.BW
         return (
                 <>
                         <Meta NameTitle="BW" linkTitle="WB" />
                         <Headers />
                         <div className="card-cantainer">
-                                {CardNetflix.map((e) => {
-                                        return <Card key={e.id} poster={e.image} summary={e.summary} name={e.name} />
+                                {BW.map((e: any) => {
+                                        return <Card key={e.id} poster={e.poster.url} videoPoster={e.mp4.url} summary={e.description} name={e.title} />
                                 })}
                         </div>
                         <Footer />
